a06df8dd3d2195e99c0aa5d5ceaadc2e249c40c6
madslavxx <78366764+madslavxx@users.noreply.github.com>
2021-02-20 13:14:20 -0500

    First person Code
    The new movement code

---
+++ Controls.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controls : MonoBehaviour
+{
+    public Rigidbody pc2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    if(Input.GetKey("space")) 
+        {
+            pc2.AddForce(0, 4, 0);
+        }
+    if (Input.GetKey("d"))
+        {
+            pc2.AddForce(5, 0, 0);
+        }
+    if (Input.GetKey("a"))
+        {
+            pc2.AddForce(-5, 0, 0);
+        }
+        if (Input.GetKey("w"))
+        {
+            pc2.AddForce(0, 5, 0);
+        }
+    }
+}
